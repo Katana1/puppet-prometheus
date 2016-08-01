a77fc70d9953bbe566d6512488b959412816deb4
Andrew Bramble <andrew.bramble@katana1.com>
2016-08-01 16:06:45 +1000

    see if we can add the default consoles from the prometheus release tarball

---
+++ manifests/consoles.pp
@@ -0,0 +1,33 @@
+class prometheus::consoles {
+  # Not thrilling me to do it this way, since we also need to poke the config into prometheus.yaml
+  include staging
+  staging::file { "prometheus-${prometheus::version}.${prometheus::download_extension}":
+    source => $prometheus::real_download_url,
+  } ->
+  file { "${::staging::path}/prometheus-${prometheus::version}":
+    ensure => directory,
+  } ->
+  staging::extract { "prometheus-${prometheus::version}.${prometheus::download_extension}":
+    target  => $::staging::path,
+    creates => "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/prometheus",
+  } ->
+  file {
+    "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/consoles":
+      owner => 'root',
+      group => 0, # 0 instead of root because OS X uses "wheel".
+      mode  => '0555';
+    "${::prometheus::config_dir}/consoles":
+      ensure => link,
+      target => "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/consoles";
+  } ->
+  file {
+    "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/console_libraries":
+      owner => 'root',
+      group => 0, # 0 instead of root because OS X uses "wheel".
+      mode  => '0555';
+    "${::prometheus::config_dir}/console_libraries":
+      ensure => link,
+      target => "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/console_libraries";
+  }
+
+}
