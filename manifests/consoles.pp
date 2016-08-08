19f5f74968a90f7223911a4740e15231b6d0a2ef
Andrew Bramble <andrew.bramble@katana1.com>
2016-08-08 13:43:53 +1000

    AX-52 unpack the console templates as provided by prometheus

--- manifests/consoles.pp
+++ manifests/consoles.pp
@@ -2,12 +2,6 @@ class prometheus::consoles {
   # Not thrilling me to do it this way, since we also need to poke the config into prometheus.yaml
   include staging
 
-
-  staging::extract { "prometheus-${prometheus::version}.${prometheus::download_extension}":
-    target  => $::staging::path,
-    creates => "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/consoles",
-    require =>   File["${::staging::path}/prometheus-${prometheus::version}"]
-  } ->
   file {
     "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/consoles":
       owner => 'root',
@@ -15,6 +9,7 @@ class prometheus::consoles {
       mode  => '0555';
     "${::prometheus::config_dir}/consoles":
       ensure => link,
+      require => Class['prometheus'],
       target => "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/consoles";
   } ->
   file {
@@ -26,5 +21,8 @@ class prometheus::consoles {
       ensure => link,
       target => "${::staging::path}/prometheus-${prometheus::version}.${prometheus::os}-${prometheus::arch}/console_libraries";
   }
+
+  # Cludge together extra arguments for launching prometheus server
+  $::prometheus::extra_options = "${::prometheus::extra_options} -web.console.templates=/etc/prometheus/consoles -web.console.libraries=/etc/prometheus/console_libraries"
 
 }
